f1307df3fd5caad063c48084af31c926756523cc
Jackie Maloney <jmaloney11277@gmail.com>
2024-10-31 20:07:07 -0400

    fill out auth.ts to-dos

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,46 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (token) {
+      return jwtDecode<JwtPayload>(token);
+    }
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    if (token) {
+      return true;
+    }
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decoded = jwtDecode<JwtPayload>(token);
+    if (decoded.exp && decoded.exp * 1000 < Date.now()) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
