0d18d42f55b7e781e1eac7af8e245b367571b9d0
Jackie Maloney <jmaloney11277@gmail.com>
2024-11-02 17:21:26 -0400

    adding things for render deployment

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,27 @@
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// import { Sequelize } from 'sequelize';
+// import { UserFactory } from './user.js';
+// import { TicketFactory } from './ticket.js';
+
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// const User = UserFactory(sequelize);
+// const Ticket = TicketFactory(sequelize);
+
+// User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
+// Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+
+// export { sequelize, User, Ticket };
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,19 +30,37 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
       dialectOptions: {
-        decimalNumbers: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // This is important for managed environments like Render
+        },
       },
-    });
+    })
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD || '',
+      {
+        host: 'localhost',
+        dialect: 'postgres',
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+          decimalNumbers: true,
+        },
+      }
+    );
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };
+
